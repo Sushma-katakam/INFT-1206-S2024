9807c561042d90674bf68fbf9249f5695bd6acd7
Sushma-katakam <sushma.katakam@dcmail.ca>
2024-08-06 16:36:32 -0400

    Update main.js

---
+++ Assignments/Assignment_4/Part_1/main.js
@@ -0,0 +1,9 @@
+/*
+Name: Sushma Katakam
+File:main.js
+Date :06 august 2024
+Description: Assignment 4 part 1
+*/
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
