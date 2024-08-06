fa53184ffc82689be8b177b526444200e0dbbe87
Sushma-katakam <sushma.katakam@dcmail.ca>
2024-08-06 16:37:56 -0400

    Update main.js

--- Assignments/Assignment_4/Part_1/main.js
+++ Assignments/Assignment_4/Part_1/main.js
@@ -7,3 +7,8 @@ Description: Assignment 4 part 1
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+    const random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
