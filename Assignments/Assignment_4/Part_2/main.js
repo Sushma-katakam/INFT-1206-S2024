7f052fdcb62d2359e253e02acd2bb8e9e5354326
Sushma-katakam <sushma.katakam@dcmail.ca>
2024-08-08 08:49:09 -0400

    Update main.js

--- Assignments/Assignment_4/Part_2/main.js
+++ Assignments/Assignment_4/Part_2/main.js
@@ -1,3 +1,10 @@
+/*
+    Name : Sushma Katakam
+    Date : 08-08-2024
+    File : main.js
+    Description : Assignment 4 part 2
+
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,10 +12,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const alt = ["pic 1", "pic 2", "pic 3", "pic 4", "pic 5"];
 
 /* Looping through images */
+
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
