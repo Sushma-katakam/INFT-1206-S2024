37062fd46aac7def89cb7ed865208aa3806debf1
Sushma-katakam <sushma.katakam@dcmail.ca>
2024-08-08 15:14:18 -0400

    main.js

---
+++ Assignments/Assignment_4/part_3/main.js
@@ -0,0 +1,25 @@
+/*
+Name:Sushma Katakam
+Date: 08-08-2024
+File: main.js
+Description: Bouncy balls
+*/
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
