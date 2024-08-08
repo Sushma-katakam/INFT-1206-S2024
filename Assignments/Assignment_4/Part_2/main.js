1aacd5bce7f8dda631694d49b89c4c64a313ca19
Sushma-katakam <sushma.katakam@dcmail.ca>
2024-08-08 08:51:43 -0400

    Update main.js

--- Assignments/Assignment_4/Part_2/main.js
+++ Assignments/Assignment_4/Part_2/main.js
@@ -30,9 +30,5 @@ for (const image of images) {
     })
 }
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
